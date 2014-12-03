f06717243c75f4d5ef563373b110479c0b1ed841
plodczyks <slawekplodczyk@onet.eu>
2014-12-03 23:14:15 +0100

    t2

--- paranormalGolf/app/src/main/java/mini/paranormalgolf/TestActivity.java
+++ paranormalGolf/app/src/main/java/mini/paranormalgolf/TestActivity.java
@@ -1,12 +1,13 @@
 package mini.paranormalgolf;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TestActivity extends ActionBarActivity {
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
