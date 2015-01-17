41a69ce4081b00ff45aece9e7571a2fe54bbca1a
mateusz <matkaczmarski@interia.pl>
2015-01-17 20:57:05 +0100

    Ciąg dalszy dokumentacji kodu + powrotna zmiana wywołania "updater.setContext(activity);" przy onSurfaceCreate

--- paranormalGolf/app/src/main/java/mini/paranormalgolf/Graphics/ShaderPrograms/DepthMapShaderProgram.java
+++ paranormalGolf/app/src/main/java/mini/paranormalgolf/Graphics/ShaderPrograms/DepthMapShaderProgram.java
@@ -7,14 +7,28 @@ import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniformMatrix4fv;
 
 /**
- * Created by Mateusz on 2015-01-03.
+ * Opisuje program do generowania mapy głębokości cieni.
  */
 public class DepthMapShaderProgram extends ShaderProgram {
+
+    /**
+     * Określa lokalizację stałej wartości (uniform macierzy modelViewProjection) występującej w kodzie programu.
+     */
     private final int uMVPMatrixLocation;
+    /**
+     * Określa lokalizację stałej wartości (uniform macierzy modelView) występującej w kodzie programu.
+     */
     private final int uMVMatrixLocation;
 
+    /**
+     * Określa lokalizację atrybutu pozycji wierzchołków występującego w kodzie programu.
+     */
     private final int aPositionLocation;
 
+    /**
+     * Tworzy obiekt programu do generowania mapy głębokości cieni.
+     * @param context Bieżący kontekst pozwalający uzyskać dostęp do zasobów aplikacji.
+     */
     public DepthMapShaderProgram(Context context){
         super(context, R.raw.depthmap_vertex_shader, R.raw.depthmap_fragment_shader);
         uMVPMatrixLocation = glGetUniformLocation(program, U_MVPMATRIX);
@@ -22,11 +36,20 @@ public class DepthMapShaderProgram extends ShaderProgram {
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
     }
 
+    /**
+     * Przypisuje stałym wartościom (uniformom) występującym w kodzie programu odpowiednie wartości.
+     * @param mvpMatrix Przypisywana wartość macierzy modelViewProjection dla światła.
+     * @param mvMatrix Przypisywana wartość macierzy modelView.
+     */
     public void setUniforms(float[] mvpMatrix, float[] mvMatrix) {
         glUniformMatrix4fv(uMVPMatrixLocation, 1, false, mvpMatrix, 0);
         glUniformMatrix4fv(uMVMatrixLocation, 1, false, mvMatrix, 0);
     }
 
+    /**
+     * Zwraca lokalizację atrybutu pozycji wierzchołków.
+     * @return Wartość <em><b>aPositionLocation</b></em>.
+     */
     public int getPositionAttributeLocation() {
         return aPositionLocation;
     }
--- paranormalGolf/app/src/main/java/mini/paranormalgolf/Graphics/ShaderPrograms/SkyboxShaderProgram.java
+++ paranormalGolf/app/src/main/java/mini/paranormalgolf/Graphics/ShaderPrograms/SkyboxShaderProgram.java
@@ -14,13 +14,28 @@ import static android.opengl.GLES20.glUniform1i;
 import static android.opengl.GLES20.glUniformMatrix4fv;
 
 /**
- * Created by Mateusz on 2014-12-14.
+ * Opisuje program do generowania przestrzennego tła.
  */
 public class SkyBoxShaderProgram extends ShaderProgram {
+
+    /**
+     * Określa lokalizację stałej wartości (uniform macierzy modelViewProjection) występującej w kodzie programu.
+     */
     private final int uMatrixLocation;
+    /**
+     * Określa lokalizację stałej wartości (uniform  tekstury) występującej w kodzie programu.
+     */
     private final int uTextureUnitLocation;
+
+    /**
+     * Określa lokalizację atrybutu pozycji wierzchołków występującego w kodzie programu.
+     */
     private final int aPositionLocation;
 
+    /**
+     * Tworzy obiekt programu do generowania przestrzennego tła.
+     * @param context Bieżący kontekst pozwalający uzyskać dostęp do zasobów aplikacji.
+     */
     public SkyBoxShaderProgram(Context context) {
         super(context, R.raw.skybox_vertex_shader,
                 R.raw.skybox_fragment_shader);
@@ -30,6 +45,11 @@ public class SkyBoxShaderProgram extends ShaderProgram {
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
     }
 
+    /**
+     * Przypisuje stałym wartościom (uniformom) występującym w kodzie programu odpowiednie wartości.
+     * @param matrix Przypisywana wartość macierzy modelViewProjection.
+     * @param textureId Przypisywana wartość identyfikatora tekstury.
+     */
     public void setUniforms(float[] matrix, int textureId) {
         glUniformMatrix4fv(uMatrixLocation, 1, false, matrix, 0);
 
@@ -38,6 +58,10 @@ public class SkyBoxShaderProgram extends ShaderProgram {
         glUniform1i(uTextureUnitLocation, 0);
     }
 
+    /**
+     * Zwraca lokalizację atrybutu pozycji wierzchołków.
+     * @return Wartość <em><b>aPositionLocation</b></em>.
+     */
     public int getPositionAttributeLocation() {
         return aPositionLocation;
     }
